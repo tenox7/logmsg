df71ed75f51f72a93386e99c4393608824c730af
Antoni Sawicki <as@tenoware.com>
2019-03-29 15:49:38 -0700

    Update logmsg_unix.c

--- logmsg_unix.c
+++ logmsg_unix.c
@@ -1,34 +1,48 @@
 // logmsg - output to stdout prefixed with current time
 
 #include <stdio.h>
-#include <time.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <limits.h>
 
-void logmsg(char *msg, ...) {
+enum { INFO, WARNING, ERROR };
+
+void logmsg(int s, char *msg, ...) {
     va_list ap;
     time_t t;
     struct tm *l;
+    char *err[] = { "", " WARNING:", " ERROR:" };
+    char hostname[255]={0};
     
-    time(&t);
+    time(&t); 
     l=localtime(&t);
-    
-    printf("%04d/%02d/%02d %02d:%02d:%02d: ", 
-        l->tm_year+1900, 
-        l->tm_mon+1, 
-        l->tm_mday, 
-        l->tm_hour, 
-        l->tm_min, 
-        l->tm_sec
+    gethostname(hostname, 255);
+    if(s>2)
+        s=2;
+    printf("%04d/%02d/%02d %02d:%02d:%02d %s:%s ", 
+        l->tm_year+1900, l->tm_mon+1, l->tm_mday, 
+        l->tm_hour, l->tm_min, l->tm_sec, 
+        hostname, err[s]
     );
     va_start(ap, msg);
     vprintf(msg, ap);
     va_end(ap);
     putchar('\n');
+    if(s==2)
+        exit(1);
 }
 
 int main() {
 
-    logmsg("hello %s %c", "world", '!');
+    logmsg(INFO, "hello %s %c", "world", '!');
+
+    logmsg(WARNING, "oh %s %c", "no", '!');
+
+    logmsg(ERROR, "oh %s %c", "shit", '!');
+
+    logmsg(INFO, "hello %s %c", "world", '!'); 
     
     return 0;
 }
