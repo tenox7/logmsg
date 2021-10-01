8a248a95122ac45ddf93ef62b0ca40d6fb7d0ea2
Antoni Sawicki <tenox@google.com>
2021-10-01 02:03:37 -0700

    refactor

--- logmsg_windowsW.c
+++ logmsg_windowsW.c
@@ -5,15 +5,14 @@ enum { LINFO, LWARNING, LERROR };
 
 void logmsg(int s, LPWSTR msg, ...) {
     va_list ap;
-    LPWSTR errBuff=NULL;
-    LPWSTR n=NULL;
+    WCHAR errBuff[1024] = { 0 };
     DWORD e;
     SYSTEMTIME l;
-    LPWSTR err[] = { L"", L" WARNING:", L" ERROR:" };
+    LPWSTR err[] = { L"INFO", L"WARNING", L"ERROR" };
 
     GetLocalTime(&l);
-    wprintf(L"%04d/%02d/%02d %02d:%02d:%02d %s:%s ", 
-        l.wYear, l.wMonth, l.wDay, 
+    wprintf(L"%04d/%02d/%02d %02d:%02d:%02d %s %s: ",
+        l.wYear, l.wMonth, l.wDay,
         l.wHour, l.wMinute, l.wSecond,
         _wgetenv(L"COMPUTERNAME"), err[s]
     );
@@ -22,28 +21,29 @@ void logmsg(int s, LPWSTR msg, ...) {
     vwprintf(msg, ap);
     va_end(ap);
 
-    if(s>2) s=2;
-    if(s<0) s=0;
+    if (s > 2) s = 2;
+    if (s < 0) s = 0;
 
-    if(s) {
-        e=GetLastError();
-        if(e) {
-            FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, 
-                NULL, e, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR) &errBuff, 64, NULL);
-            n=wcschr(errBuff, L'\r');
-            if(n)
-                *n=L'\0';
-            wprintf(L" [%s (0x%08X)]", errBuff, e);
-        }
+    if (!s) {
+        putwchar(L'\n');
+        return;
     }
+
+    if (e = GetLastError()) {
+        FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK,
+            NULL, e, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), errBuff, sizeof(errBuff)/sizeof(WCHAR), NULL);
+        wprintf(L" [%s (0x%08X)]", errBuff, e);
+    }
+
     putwchar(L'\n');
-    if(s==LERROR)
+
+    if (s == LERROR)
         ExitProcess(1);
 }
 
 
 
-int wmain(int argc, WCHAR **argv) {
+int wmain(int argc, WCHAR** argv) {
 
     logmsg(LINFO, L"hello %s %c", L"world", L'!');
 
@@ -53,7 +53,7 @@ int wmain(int argc, WCHAR **argv) {
 
     logmsg(LERROR, L"oh %s %c", L"shit", L'!');
 
-    logmsg(LINFO, L"this did not happen"); 
+    logmsg(LINFO, L"this did not happen");
 
     return 0;
 }
